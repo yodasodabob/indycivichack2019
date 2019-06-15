a4f009278a29740a54cf8b63157c0d9f84a97122
Sari S <sestissi16@earlham.edu>
2019-06-15 03:37:17 -0400

    Set up a basic react bootstrap nav and fixed css issues

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Launcher} from 'react-chat-window'
+import {Launcher} from 'react-chat-window';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="WebsiteTitle">
-          <text>Mandela Effect</text>
-          <div class="tab">
-            <button class="tablinks" onclick="openCity(event, 'Whatever')">Whatever</button>
-            <button class="tablinks" onclick="openCity(event, 'About)"> About</button>
-            <button class="tablinks" onclick="openCity(event, 'Whatadoop')"> Potato</button>
-          </div>
-        </div>
-        <div className="NavigationBar" height="200">
-          <text>Welcome</text>  
+        {/* <div className="WebsiteTitle">
+          <h1>Mandela Effect</h1>
+          
+        </div> */}
+        <div className="NavigationBar">
+          {/* <div className="tab">
+            <button className="tablinks" onclick="openCity(event, 'Whatever')">Whatever</button>
+            <button className="tablinks" onclick="openCity(event, 'About)"> About</button>
+            <button className="tablinks" onclick="openCity(event, 'Whatadoop')"> Potato</button>
+          </div> */}
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Navbar>
         </div>
       </header>
       <body className="App-body">
