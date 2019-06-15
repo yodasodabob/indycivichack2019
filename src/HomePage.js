931c98d4b7324d3a3ee18fd8954c2983a4d348ac
Sari S <sestissi16@earlham.edu>
2019-06-15 14:05:24 -0400

    Changing link color and adding stuff to home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,22 +2,31 @@ import React from 'react';
 import HeaderComponent from './HeaderComponent';
 import './HomePage.css';
 import {Launcher} from 'react-chat-window';
+import logoHome from './seedling-sprout-image-cropped.jpg';
 
 export class HomePage extends React.Component{
     render(){
         return(
             <div className="homePageWrapper">
             <div className="homeBody">
-                <div className="introPara">
-                <h1>The path to re-entry shouldn't be hard; we're here
-                  to make it easier.</h1>
-                <p> 
-                   With the resources we have gathered, we
-                  are dedicated in helping those who have been incarcerated
-                  get the right step in for a better future.
-
-                </p>
+                <div className="TitleLogo">
+                    <img
+                        alt=""
+                        src={logoHome}
+                        className="pictureLogoHome"
+                    />
                 </div>
+                <div className="introPara">
+                    <h1>The path to re-entry shouldn't be hard; we're here
+                    to make it easier.</h1>
+                    <p> 
+                    With the resources we have gathered, we
+                    are dedicated in helping those who have been incarcerated
+                    get the right step in for a better future.
+
+                    </p>
+                </div>
+               
             </div>
             </div>
         )
