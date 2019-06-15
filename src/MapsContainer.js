4611e62cc7ff1de98a48f5f2a910c732288f98e5
Laurence Ruberl <laurence.ruberl@cleanslatetg.com>
2019-06-15 11:39:35 -0400

    Add current location marker

--- src/MapsContainer.js
+++ src/MapsContainer.js
@@ -2,15 +2,35 @@ import config from './config.js'
 import React from 'react'
 import { GoogleApiWrapper, Map, Marker } from 'google-maps-react'
 import IndyBusJson from './data/IndyGo_Bus_Stops.json'
+import customIcon from './images/star.png'
 
 const mapsURLPre = "https://www.google.com/maps/"
 const mapsAPISnippet = "?api=1"
 const buildDirReqLatLong = (lat, long) => {
-    return(mapsURLPre+"dir/"+mapsAPISnippet+`&origin=${lat},${long}`)
+    return(mapsURLPre+"dir/"+mapsAPISnippet+`&destination=${lat},${long}`)
 }
 
 export class MapsContainer extends React.Component {
-    state = {
+    
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            activeMarker: null,
+            infoWindowContents: null,
+            infoWindowShowing: false,
+            currentLocation: null,
+        }
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                this.setState({currentLocation:{ lat: position.coords.latitude, lng: position.coords.longitude}})
+            })
+        } else {
+            console.log("Location services not allowed")
+        }
+    }
+
+    componentDidMount() {
 
     }
     
@@ -32,7 +52,7 @@ export class MapsContainer extends React.Component {
         }
         return (
             <div>
-                <Map style={style} google={this.props.google} initialCenter={{ lat: 39.768610, lng: -86.157370 }}>
+                <Map style={style} google={this.props.google} initialCenter={this.state.currentLocation===null ? { lat: 39.768610, lng: -86.157370 } : this.state.currentLocation }>
                     {displayData.map((item) => {
                         return <Marker 
                             key={item["properties"]["OBJECTID"]} 
@@ -40,6 +60,19 @@ export class MapsContainer extends React.Component {
                             position={{ lng: item["geometry"]["coordinates"][0], lat: item["geometry"]["coordinates"][1]}}
                             onClick={this.props.markerHandler} />
                     })}
+                    {this.state.currentLocation ? 
+                    <Marker 
+                        name="Your Location" 
+                        position={this.state.currentLocation} 
+                        onClick={this.props.markerHandler} 
+                        icon={{
+                            url: customIcon,
+                            anchor: new this.props.google.maps.Point(32,32),
+                            scaledSize: new this.props.google.maps.Size(64,64)
+                        }}
+                        />  
+                    : 
+                    null}
                 </Map>
             </div>
         )
