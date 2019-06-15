8e84b6603dbe4ea6acbbdc58772e5d3a4a7ef9a8
Priscilla Coronado <priscillacoron@gmail.com>
2019-06-14 23:55:51 -0400

    Chat package - does not have words though

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Launcher} from 'react-chat-window'
 
 function App() {
   return (
@@ -14,6 +15,16 @@ function App() {
         </div>
       </header>
       <body className="App-body">
+
+      <Launcher
+        agentProfile={{
+          teamName: 'react-chat-window',
+          imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
+        }}
+        // onMessageWasSent={this._onMessageWasSent.bind(this)} Add messages
+        // messageList={this.state.messageList}
+        showEmoji
+      />
 
       </body>
     </div>
