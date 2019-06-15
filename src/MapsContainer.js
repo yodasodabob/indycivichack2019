55a0b0df3617b783d30a08c2bca60a874e35a165
Laurence Ruberl <laurence.ruberl@cleanslatetg.com>
2019-06-15 12:32:40 -0400

    Add info window

--- src/MapsContainer.js
+++ src/MapsContainer.js
@@ -1,6 +1,6 @@
 import config from './config.js'
 import React from 'react'
-import { GoogleApiWrapper, Map, Marker } from 'google-maps-react'
+import { GoogleApiWrapper, Map, Marker, InfoWindow } from 'google-maps-react'
 import IndyBusJson from './data/IndyGo_Bus_Stops.json'
 import customIcon from './images/star.png'
 
@@ -8,6 +8,14 @@ const mapsURLPre = "https://www.google.com/maps/"
 const mapsAPISnippet = "?api=1"
 const buildDirReqLatLong = (lat, long) => {
     return(mapsURLPre+"dir/"+mapsAPISnippet+`&destination=${lat},${long}`)
+}
+const buildInfoWindowContents = (name, lat, long) => {
+    return(
+        <React.Fragment>
+            <h5>{name}</h5>
+            <p><a href={buildDirReqLatLong(lat, long)}>Click here for directions!</a></p>
+        </React.Fragment>
+    )
 }
 
 export class MapsContainer extends React.Component {
@@ -20,6 +28,8 @@ export class MapsContainer extends React.Component {
             infoWindowContents: null,
             infoWindowShowing: false,
             currentLocation: null,
+            selectedLocation: null,
+            selectedLocDir: null,
         }
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((position) => {
@@ -30,13 +40,19 @@ export class MapsContainer extends React.Component {
         }
     }
 
-    componentDidMount() {
+    markerHandler(props, marker, ev) {
+        const position = props.position;
+        this.setState({ activeMarker: marker, selectedLocation: props.name, selectedLocDir: buildDirReqLatLong(position.lat, position.lng), infoWindowContents: buildInfoWindowContents(props.name, position.lat, position.lng), infoWindowShowing: true})
+        console.log(props, marker, ev)
+    }
 
+    hideInfoWindow(){
+        if (this.state.infoWindowShowing === true) {
+            this.setState({ infoWindowShowing: false })
+        }
     }
     
     render() {
-        console.log(IndyBusJson)
-        console.log(this.props.testProp)
         const indyBusData = IndyBusJson["features"];
         const displayData = this.props.displayData
         const markerClickHandler = (props, marker, ev) => {
@@ -52,19 +68,19 @@ export class MapsContainer extends React.Component {
         }
         return (
             <div>
-                <Map style={style} google={this.props.google} initialCenter={this.state.currentLocation===null ? { lat: 39.768610, lng: -86.157370 } : this.state.currentLocation }>
+                <Map style={style} onClick={this.hideInfoWindow.bind(this)} google={this.props.google} initialCenter={this.state.currentLocation===null ? { lat: 39.768610, lng: -86.157370 } : this.state.currentLocation }>
                     {displayData.map((item) => {
                         return <Marker 
                             key={item["properties"]["OBJECTID"]} 
                             name={item["properties"]["DESCRIPTION"]}
                             position={{ lng: item["geometry"]["coordinates"][0], lat: item["geometry"]["coordinates"][1]}}
-                            onClick={this.props.markerHandler} />
+                            onClick={this.markerHandler.bind(this)} />
                     })}
                     {this.state.currentLocation ? 
                     <Marker 
                         name="Your Location" 
                         position={this.state.currentLocation} 
-                        onClick={this.props.markerHandler} 
+                        onClick={this.markerHandler.bind(this)} 
                         icon={{
                             url: customIcon,
                             anchor: new this.props.google.maps.Point(32,32),
@@ -73,6 +89,9 @@ export class MapsContainer extends React.Component {
                         />  
                     : 
                     null}
+                    <InfoWindow marker={this.state.activeMarker} visible={this.state.infoWindowShowing}>
+                        {this.state.infoWindowContents}
+                    </InfoWindow>
                 </Map>
             </div>
         )
