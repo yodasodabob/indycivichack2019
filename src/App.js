b2beb88336453154b27bde742b0404f1f22d53de
Laurence Ruberl <laurence.ruberl@cleanslatetg.com>
2019-06-15 03:20:36 -0400

    Add maps integration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GoogleApiWrapper from './MapsContainer';
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <GoogleApiWrapper></GoogleApiWrapper>
+      </div>
     </div>
   );
 }
--- src/MapsContainer.js
+++ src/MapsContainer.js
@@ -1,15 +1,32 @@
 import config from './config.js'
 import React from 'react'
-import { GoogleApiWrapper } from 'google-maps-react'
+import { GoogleApiWrapper, Map, Marker } from 'google-maps-react'
+import IndyBusJson from './data/IndyGo_Bus_Stops.json'
 
 export class MapsContainer extends React.Component {
+    state = {
+
+    }
+    
     render() {
-        if (!this.props.loaded) {
-            return <div>Loading...</div>
+        console.log(IndyBusJson)
+        console.log(this.props.testProp)
+        const indyBusData = IndyBusJson["features"];
+        // console.log(indyBusData)
+        // const indyBusExtract = Object.keys(Object.keys(indyBusData)[1]).map((val, index) => {})
+        const style = {
+            width: '100%',
+            height: '100%',
         }
-            return (
-                <div>Map will go here</div>
-            )
+        return (
+            <div>
+                <Map style={style} google={this.props.google} initialCenter={{ lat: 39.768610, lng: -86.157370 }}>
+                    {indyBusData.map((item) => {
+                        return <Marker key={item["properties"]["OBJECTID"]} name={item["properties"]["DESCRIPTION"]} position={{ lng: item["geometry"]["coordinates"][0], lat: item["geometry"]["coordinates"][1]}} />
+                    })}
+                </Map>
+            </div>
+        )
     }
 }
 
