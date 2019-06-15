323ecdc3e1c165cb5b8bdea98ce6e4da56b9f5c2
Sari S <sestissi16@earlham.edu>
2019-06-15 09:21:36 -0400

    Basic Navigation Function

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import React from 'react';
 import HeaderComponent from './HeaderComponent';
 import './App.css';
 import {Launcher} from 'react-chat-window';
+import { Route, Switch } from 'react-router-dom';
 
 import GoogleApiWrapper from './MapsContainer';
+import HomePage from './HomePage';
 
 function App() {
   return (
     <div className="App">
       <div><HeaderComponent></HeaderComponent></div>
+      <div className="SwitchDiv">
+        <Switch>
+          <Route path="/map" render={() => <div>This is a thing!</div>} />
+          <Route path='/' render={() =><div>
+            <HomePage></HomePage>
+          </div>} />
+        </Switch>
+      </div>
       <div>
         <GoogleApiWrapper></GoogleApiWrapper>
       </div>
       <body className="App-body">
 
-      <Launcher
+      {/* <Launcher
         agentProfile={{
           teamName: 'react-chat-window',
           imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
@@ -22,7 +32,7 @@ function App() {
         // onMessageWasSent={this._onMessageWasSent.bind(this)} Add messages
         // messageList={this.state.messageList}
         showEmoji
-      />
+      /> */}
 
       </body>
     </div>
